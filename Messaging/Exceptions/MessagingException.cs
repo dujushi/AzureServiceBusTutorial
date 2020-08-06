557d15ae7ed53bccdf7057abe33c3d5f97befb53
William Guo <dujushi@gmail.com>
2020-08-07 11:50:32 +1200

    Add project files.

---
+++ Messaging/Exceptions/MessagingException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace Messaging.Exceptions
+{
+    /// <summary>
+    /// Represents exceptions related to messaging services.
+    /// </summary>
+    [Serializable]
+    public class MessagingException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagingException"/> class.
+        /// </summary>
+        public MessagingException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagingException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        public MessagingException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagingException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        /// <param name="inner">The inner exception</param>
+        public MessagingException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagingException"/> class.
+        /// </summary>
+        /// <param name="info">The serialization info</param>
+        /// <param name="context">The context</param>
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        protected MessagingException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ Messaging/ITopicPublisher.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Messaging
+{
+    /// <summary>
+    /// Represents an object to send message to a topic queue.
+    /// </summary>
+    /// <typeparam name="T">The type of the message</typeparam>
+    public interface ITopicPublisher<in T>
+    {
+        /// <summary>
+        /// Sends a message to a topic queue.
+        /// </summary>
+        /// <param name="topicMessage">The message</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task SendAsync(T topicMessage);
+    }
+}
